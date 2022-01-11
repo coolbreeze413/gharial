284e4770ba1d2846618c539d18bff9043e0d5eb2
coolbreeze413 <coolbreeze413@yahoo.co.in>
2022-01-11 16:03:01 +0530

    cleanup script flow

--- generate_release.sh
+++ generate_release.sh
@@ -8,36 +8,52 @@ CURRENT_TIME=$(date +"%H_%M_%S")
 GH_OWNER="coolbreeze413"
 GH_REPO="gharial"
 
-#echo "$CURRENT_DATE"
-#echo "$CURRENT_TIME"
+# bash variables
+TRUE_VAL="TRUE"
+FALSE_VAL="FALSE"
 
-MODE_CREATE_PR_ONLY="false"
-if [ "$#" == "1" ] ; then
-    if [ "$1" == "create-pr" ] ; then
-        MODE_CREATE_PR_ONLY="true"
+echo "DATE: $CURRENT_DATE"
+echo "TIME: $CURRENT_TIME"
+
+##########################################################################################
+# parse args
+##########################################################################################
+# gha mode: if enabled, create PR and wait for gh actions to publish release
+# if not specified, then we are in standalone mode, and this script will
+# also merge PR and publish release without triggering gh actions.
+GHA_MODE="github-actions"
+GHA_MODE_ENABLED="$FALSE_VAL"
+
+if [ $# == 1 ] ; then
+
+    if [ "$1" == "$GHA_MODE" ] ; then
+
+        echo ""
+        echo "[>> GHARIAL <<] GHA MODE specified!"
+        echo ""
+
+        GHA_MODE_ENABLED="$TRUE_VAL"
+
     fi
+
 fi
+##########################################################################################
 
-# spinner stuff
 
+##########################################################################################
+# STEP 1 : pull in all the changes from the master branch
+##########################################################################################
+git pull
+
+
+##########################################################################################
+# STEP 2 : processing and generate artifact
+##########################################################################################
 set_spinner_snakey() {
 
     FRAME=("⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏")
     FRAME_INTERVAL=0.1
 }
-
-set_spinner_infinity() {
-
-    FRAME=("⚬" "⚭" "⚮" "⚯")
-    FRAME_INTERVAL=0.25
-}
-
-set_spinner_rotatingbar() {
-
-    FRAME=("-" "\\" "|" "/")
-    FRAME_INTERVAL=0.25
-}
-
 
 CMD_WITH_SPINNER=
 CMD_WITH_SPINNER_MSG=
@@ -62,17 +78,12 @@ execute_cmd_spinner() {
     tput cnorm -- normal
 }
 
-# STEP 0: pull all remote repo changes in case we are running from an already cloned repo:
-git pull
-
-
-# STEP 1: generate dummy release:
 
 RELEASE_NAME="gharial_release_${CURRENT_DATE}_${CURRENT_TIME}.sh"
 RELEASES_DIR="${GHARIAL_ROOT_DIR}/release-artifacts"
 
 echo
-echo "creating new release artifact: $RELEASE_NAME"
+echo "creating new release artifact: $RELEASE_NAME in $RELEASES_DIR"
 echo
 
 # pretend we are busy
@@ -80,12 +91,11 @@ CMD_WITH_SPINNER="sleep 3"
 CMD_WITH_SPINNER_MSG="hard at work"
 set_spinner_snakey
 execute_cmd_spinner
-echo $OUPUT
 
 # pretend we are busy
 CMD_WITH_SPINNER="sleep 3"
 CMD_WITH_SPINNER_MSG="doing some more work"
-set_spinner_rotatingbar
+set_spinner_snakey
 execute_cmd_spinner
 
 # generate artifact
@@ -97,52 +107,65 @@ echo "" >> "$RELEASE_NAME"
 echo "echo \"$RELEASE_NAME\"" >> "$RELEASE_NAME"
 echo "" >> "$RELEASE_NAME"
 
+
 # move the release artifact into the target dir:
 mv -v "$RELEASE_NAME" "${RELEASES_DIR}/${RELEASE_NAME}"
 
 
-
-# STEP 2: commit the artifact into new branch (for raising a PR)
+##########################################################################################
+# STEP 3 : commit the artifact into new branch (for raising a PR)
+##########################################################################################
 DEFAULT_BRANCH_NAME="master"
-if [ "$MODE_CREATE_PR_ONLY" == "true" ] ; then
+if [ "$GHA_MODE_ENABLED" == "$TRUE_VAL" ] ; then
     # this mode has a gh action which is triggered with a PR from a 'releases/**' branch
-    RELEASES_BRANCH_NAME="releases/${CURRENT_DATE}/${CURRENT_TIME}"
+    RELEASE_BRANCH_NAME="releases/${CURRENT_DATE}/${CURRENT_TIME}"
 else
     # preven the gh action from triggering for standalone mode, so use a different branch name than above
-    RELEASES_BRANCH_NAME="release-no-gha/${CURRENT_DATE}/${CURRENT_TIME}"
+    RELEASE_BRANCH_NAME="releases-no-gha/${CURRENT_DATE}/${CURRENT_TIME}"
 fi
 
 echo
 echo "commit release to a new branch and push upstream."
 echo "        repo: ${GH_REPO}"
-echo "      branch: ${RELEASES_BRANCH_NAME}"
+echo "      branch: ${RELEASE_BRANCH_NAME}"
 echo "     release: ${RELEASES_DIR}/${RELEASE_NAME}"
 echo
 
 echo
 echo -e "\n\n[-- 01 --]"
-git checkout -b "$RELEASES_BRANCH_NAME"
+git checkout -b "$RELEASE_BRANCH_NAME"
+
 echo -e "\n\n[-- 02 --]"
 git status
+
 echo -e "\n\n[-- 03 --]"
 git add "${RELEASES_DIR}/${RELEASE_NAME}"
+
 echo -e "\n\n[-- 04 --]"
 git status
+
 echo -e "\n\n[-- 05 --]"
 git commit -m "[GHARIAL-DECEPTICON-COM] add new release ${RELEASE_NAME}"
+
 echo -e "\n\n[-- 06 --]"
 git status
+
 echo -e "\n\n[-- 07 --]"
-git push -u origin "$RELEASES_BRANCH_NAME"
+git push -u origin "$RELEASE_BRANCH_NAME"
+
 echo -e "\n\n[-- 08 --]"
 git checkout "$DEFAULT_BRANCH_NAME"
+
 echo -e "\n\n[-- 09 --]"
 git pull
+
 echo "[-- -- --]"
 echo
 
 
-# prerequisites
+##########################################################################################
+# STEP 4 : extract PAT from git config
+##########################################################################################
 # assuming we cloned the repo using a PAT, the token is stored in .git/config file in plaintext
 # what we want is the pattern: "url = https://TOKEN@github.com/OWNER/REPO[.git]"
 # using regex:
@@ -169,6 +192,9 @@ if [ -z "$GH_CONFIG_TOKEN" ] ; then
 fi
 
 
+##########################################################################################
+# STEP 5 : setup gh-cli
+##########################################################################################
 GH_CLI_VERSION="2.4.0"
 GH_CLI_LINUX_BIN_NAME="gh_${GH_CLI_VERSION}_linux_amd64"
 if [ ! -d "$GH_CLI_LINUX_BIN_NAME" ] ; then
@@ -205,7 +231,9 @@ gh --version
 echo
 
 
-# login
+##########################################################################################
+# STEP 6 : gh-cli auth
+##########################################################################################
 echo "$GH_CONFIG_TOKEN" | gh auth login --with-token
 GH_LOGIN_STATUS=$?
 
@@ -223,10 +251,13 @@ echo "gh cli login [OK]"
 echo
 
 
-# create PR
+
+##########################################################################################
+# STEP 7 : create PR for artifact
+##########################################################################################
 PR_TITLE="[GHARIAL-DECEPTICON-PR] Add new release: ${RELEASE_NAME}"
 PR_BODY="auto create PR for adding a new release."
-PR_HEAD="$RELEASES_BRANCH_NAME"
+PR_HEAD="$RELEASE_BRANCH_NAME"
 PR_BASE="$DEFAULT_BRANCH_NAME"
 PR_URL=$(gh pr create --title "$PR_TITLE" \
              --body "$PR_BODY" \
@@ -251,72 +282,193 @@ echo "    $PR_URL"
 echo
 
 
-if [ "$MODE_CREATE_PR_ONLY" == "true" ] ; then
+##########################################################################################
+# STEP 8 : if STANDALONE MODE (**NOT** GHA MODE) - MERGE PR, CREATE RELEASE
+##########################################################################################
+if [ "$GHA_MODE_ENABLED" == "$FALSE_VAL" ] ; then
 
-    # wait for the PR to be merged, and the release created...
-    TIMEOUT=120
-    RETRY_TIME=30
-    CURR_TIME=0
-    RELEASE_ASSET=""
 
-    echo
-    echo "waiting for release to be created by GHA"
+    # approve PR (optional) - need PAT of user other than the PAT used for creating PR!
+    # (not used in script flow!)
 
-    while [ $CURR_TIME -le $TIMEOUT ] ; do
 
-        echo "[waiting]"
+    ##########################################################################################
+    # STEP 8a : merge PR (squash and merge as a single commit)
+    ##########################################################################################
+    PR_MERGE_BODY="[GHARIAL-DECEPTICON-MERGE] auto merge latest PR for release"
+    PR_MERGE_RESPONSE=$(gh pr merge $PR_URL --auto --delete-branch --squash --body "$PR_MERGE_BODY")
 
-        RELEASE_ASSET=$(gh release view --json assets --jq '.assets[].name')
-        
-        if [ "$RELEASE_ASSET" == "$RELEASE_NAME" ] ; then
-            break
-        fi
+    GH_PR_MERGE_STATUS=$?
 
-        CURR_TIME=$(($CURR_TIME + $RETRY_TIME))
-        sleep $RETRY_TIME
-
-    done
-    
-    if [ "$RELEASE_ASSET" == "$RELEASE_NAME" ] ; then
-
-        RELEASE_TAG=$(gh release view --json tagName --jq '.tagName')
-        RELEASE_URL=$(gh release view --json url --jq '.url')
-        RELEASE_ASSET_URL=$(gh release view --json assets --jq '.assets[].url')
+    if [ $GH_PR_MERGE_STATUS -ne 0 ] ; then
 
         echo
-        echo "release created by GHA [OK]"
-        echo "        RELEASE_TAG: $RELEASE_TAG"
-        echo "        RELEASE_URL: $RELEASE_URL"
-        echo "      RELEASE_ASSET: $RELEASE_ASSET"
-        echo "  RELEASE_ASSET_URL: $RELEASE_ASSET_URL"
+        echo "[ERROR] gh pr merge failed: $GH_PR_MERGE_STATUS"
+        echo "$GH_PR_MERGE_STATUS"
         echo
-
-    else
-
-        echo "[ERROR]timed out waiting for new release creation!"
-        echo "    check the GH Actions workflow for any errors!"
+        exit 1
 
     fi
 
-    # logout
-    echo "Y" | gh auth logout --hostname github.com
+    echo
+    echo "gh merge [OK]"
+    echo
 
-    # checkout default branch and fetch
+
+    ##########################################################################################
+    # STEP 8b : pull in latest remote and switch to default branch now
+    ########################################################################################### pull in latest remote and switch to default branch now
     git checkout "$DEFAULT_BRANCH_NAME"
-    git fetch --tags origin
+    git pull
 
-    # clean up branches
-    # remove pointers to remote branches that don't exist
-    git fetch --prune
-    # delete local branches which don't have remotes (merged only)
-    #git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -d
-    # force delete local branches without remotes (unmerged)
-    git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -D
 
-    # exit
-    exit 0
+
+    ##########################################################################################
+    # STEP 8c : identify new tag version to use (semver)
+    ##########################################################################################
+    # ensure to prevent error when no tags available (important in case the script is run with -e)
+    CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null || true`
+    if [ -z $CURRENT_VERSION ] ; then
+        
+        CURRENT_VERSION="v0.0.0"
+        NEW_VERSION="v1.0.0"
+
+    else
+
+        # remove "v"
+        CURRENT_VERSION_PARTS=$(echo "$CURRENT_VERSION" | sed 's/v//')
+        # replace . with space so can split into an array
+        CURRENT_VERSION_PARTS=(${CURRENT_VERSION_PARTS//./ })
+
+        # get MAJOR, MINOR, PATCH
+        V_MAJOR=${CURRENT_VERSION_PARTS[0]}
+        V_MINOR=${CURRENT_VERSION_PARTS[1]}
+        V_PATCH=${CURRENT_VERSION_PARTS[2]}
+
+        # use custom logic to determine new MAJOR/MINOR/PATCH version numbers:
+        # current we use a simple "increment minor"
+        V_MINOR=$((V_MINOR+1))
+
+        # remember to add "v"
+        NEW_VERSION="v${V_MAJOR}.${V_MINOR}.${V_PATCH}"
+
+    fi
+
+    echo "CURRENT_VERSION=$CURRENT_VERSION"
+    echo "NEW_VERSION=$NEW_VERSION"
+
+
+
+    ##########################################################################################
+    # STEP 8d : publish release
+    ##########################################################################################
+    RELEASE_NOTES_FILE="${PWD}/gh_release_changelog_tmp"
+    # if file exists, empty contents, else create file all in one! REF: https://askubuntu.com/a/549672
+    : > "$RELEASE_NOTES_FILE"
+    echo "placeholder notes content for notes or changelog !!" >> "$RELEASE_NOTES_FILE"
+    echo "blah blah notes for ${RELEASE_NAME}" >> "$RELEASE_NOTES_FILE"
+
+    RELEASE_TITLE="release ${NEW_VERSION} : ${RELEASE_NAME}"
+
+    GH_RELEASE_URL=$(gh release create --title "$RELEASE_TITLE" \
+                                       --notes-file "$RELEASE_NOTES_FILE" \
+                                       --target "$DEFAULT_BRANCH_NAME" \
+                                       "$NEW_VERSION" \
+                                       "${RELEASES_DIR}/${RELEASE_NAME}" )
+
+    GH_RELEASE_STATUS=$?
+
+
+    if [ $GH_RELEASE_STATUS -ne 0 ] ; then
+
+        echo
+        echo "[ERROR] gh release create failed: $GH_RELEASE_STATUS"
+        echo "$GH_RELEASE_URL"
+        echo
+        exit 1
+
+    fi
+
+
+fi # if [ "$GHA_MODE_ENABLED" == "$FALSE_VAL" ]
+
+
+##########################################################################################
+# STEP 9 : wait for release to be published
+##########################################################################################
+TIMEOUT=120
+RETRY_TIME=30
+CURR_TIME=0
+RELEASE_ASSET=""
+
+echo
+echo "waiting for release to be created by GHA"
+
+while [ $CURR_TIME -le $TIMEOUT ] ; do
+
+    sleep $RETRY_TIME
+
+    RELEASE_ASSET=$(gh release view --json assets --jq '.assets[].name')
+    
+    if [ "$RELEASE_ASSET" == "$RELEASE_NAME" ] ; then
+        break
+    fi
+
+    CURR_TIME=$(($CURR_TIME + $RETRY_TIME))
+    
+    echo "[waiting]"
+
+done
+
+if [ "$RELEASE_ASSET" == "$RELEASE_NAME" ] ; then
+
+    RELEASE_TAG=$(gh release view --json tagName --jq '.tagName')
+    RELEASE_URL=$(gh release view --json url --jq '.url')
+    RELEASE_ASSET_URL=$(gh release view --json assets --jq '.assets[].url')
+
+    echo
+    echo "release created by GHA [OK]"
+    echo "        RELEASE_TAG: $RELEASE_TAG"
+    echo "        RELEASE_URL: $RELEASE_URL"
+    echo "      RELEASE_ASSET: $RELEASE_ASSET"
+    echo "  RELEASE_ASSET_URL: $RELEASE_ASSET_URL"
+    echo
+
+else
+
+    echo "[ERROR]timed out waiting for new release creation!"
+    echo "    check the GH Actions workflow for any errors!"
 
 fi
+
+
+
+##########################################################################################
+# STEP 10 : cleanup
+##########################################################################################
+# logout of gh-cli
+echo "Y" | gh auth logout --hostname github.com
+
+
+# checkout default branch and fetch new tags
+git checkout "$DEFAULT_BRANCH_NAME"
+git pull
+git fetch --tags origin
+
+
+# clean up branches
+# remove pointers to remote branches that don't exist
+git fetch --prune
+# delete local branches which don't have remotes (merged only)
+#git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -d
+# force delete local branches without remotes (unmerged)
+git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -D
+
+
+# exit
+exit 0
+
+
 
 
 ##########################################################################################
@@ -404,137 +556,3 @@ fi
 #     exit 1
 # fi
 ##########################################################################################
-
-
-
-# approve PR (optional) - need PAT of user other than the PAT used for creating PR!
-# (not used in script flow!)
-
-
-
-# merge PR (squash and merge as a single commit)
-PR_MERGE_BODY="[GHARIAL-DECEPTICON-MERGE] auto merge latest PR for release"
-PR_MERGE_RESPONSE=$(gh pr merge $PR_URL --auto --delete-branch --squash --body "$PR_MERGE_BODY")
-
-GH_PR_MERGE_STATUS=$?
-
-if [ $GH_PR_MERGE_STATUS -ne 0 ] ; then
-
-    echo
-    echo "[ERROR] gh pr merge failed: $GH_PR_MERGE_STATUS"
-    echo "$GH_PR_MERGE_STATUS"
-    echo
-    exit 1
-
-fi
-
-echo
-echo "gh merge [OK]"
-echo
-
-
-
-# pull in latest remote and switch to default branch now
-git checkout "$DEFAULT_BRANCH_NAME"
-git pull
-
-
-
-# identify new tag version to use (semver)
-CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
-if [ -z $CURRENT_VERSION ] ; then
-    
-    CURRENT_VERSION="v0.0.0"
-    NEW_VERSION="v1.0.0"
-
-else
-
-    # remove "v"
-    CURRENT_VERSION_PARTS=$(echo "$CURRENT_VERSION" | sed 's/v//')
-    # replace . with space so can split into an array
-    CURRENT_VERSION_PARTS=(${CURRENT_VERSION_PARTS//./ })
-
-    # get MAJOR, MINOR, PATCH
-    V_MAJOR=${CURRENT_VERSION_PARTS[0]}
-    V_MINOR=${CURRENT_VERSION_PARTS[1]}
-    V_PATCH=${CURRENT_VERSION_PARTS[2]}
-
-    # use custom logic to determine new MAJOR/MINOR/PATCH version numbers:
-    # current we use a simple "increment minor"
-    V_MINOR=$((V_MINOR+1))
-
-    # remember to add "v"
-    NEW_VERSION="v${V_MAJOR}.${V_MINOR}.${V_PATCH}"
-
-fi
-
-echo "CURRENT_VERSION=$CURRENT_VERSION"
-echo "NEW_VERSION=$NEW_VERSION"
-
-
-
-# create release
-RELEASE_NOTES_FILE="${PWD}/gh_release_changelog_tmp"
-# if file exists, empty contents, else create file all in one! REF: https://askubuntu.com/a/549672
-: > "$RELEASE_NOTES_FILE"
-echo "placeholder notes content for notes or changelog !!" >> "$RELEASE_NOTES_FILE"
-echo "blah blah notes for ${RELEASE_NAME}" >> "$RELEASE_NOTES_FILE"
-
-RELEASE_TITLE="release ${NEW_VERSION} : ${RELEASE_NAME}"
-
-GH_RELEASE_URL=$(gh release create --title "$RELEASE_TITLE" \
-                                        --notes-file "$RELEASE_NOTES_FILE" \
-                                        --target "$DEFAULT_BRANCH_NAME" \
-                                        "$NEW_VERSION" \
-                                        "${RELEASES_DIR}/${RELEASE_NAME}" )
-
-GH_RELEASE_STATUS=$?
-
-
-if [ $GH_RELEASE_STATUS -ne 0 ] ; then
-
-    echo
-    echo "[ERROR] gh release create failed: $GH_RELEASE_STATUS"
-    echo "$GH_RELEASE_URL"
-    echo
-    exit 1
-
-fi
-
-
-# wait for some time before querying the latest release (takes some time to update in gh)
-sleep 10
-RELEASE_TAG=$(gh release view --json tagName --jq '.tagName')
-RELEASE_URL=$(gh release view --json url --jq '.url')
-RELEASE_ASSET=$(gh release view --json assets --jq '.assets[].name')
-RELEASE_ASSET_URL=$(gh release view --json assets --jq '.assets[].url')
-
-echo
-echo "release created by GHA [OK]"
-echo "        RELEASE_TAG: $RELEASE_TAG"
-echo "        RELEASE_URL: $RELEASE_URL"
-echo "      RELEASE_ASSET: $RELEASE_ASSET"
-echo "  RELEASE_ASSET_URL: $RELEASE_ASSET_URL"
-echo
-
-
-
-# fetch the new tags after release:
-git fetch --tags origin
-
-
-
-# clean up branches
-# remove pointers to remote branches that don't exist
-git fetch --prune
-# delete local branches which don't have remotes (merged only)
-#git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -d
-# force delete local branches without remotes (unmerged)
-git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -D
-
-
-
-# logout
-echo "Y" | gh auth logout --hostname github.com
-
-exit 0
