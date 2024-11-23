86caef5b504f63246493b629b29d31350598659a
coolbreeze413 <coolbreeze413@yahoo.co.in>
2024-11-23 10:02:16 +0530

    another try

---
+++ test1.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+WHICHTAG="v2.8.0"
+au_device_package_pattern="testarray/PACKAGEPREFIX_*_en.tar.gz"
+DEVICE_PACKAGES_ARRAY=($(ls ${au_device_package_pattern}))
+NUM_FILES=${#DEVICE_PACKAGES_ARRAY[*]}
+echo ">>> found : ${NUM_FILES} ${au_device_package_pattern} files"
+
+echo
+echo "array:"
+echo ${DEVICE_PACKAGES_ARRAY[*]}
+echo
+echo
+
+
+i=0
+while [ $i -lt ${NUM_FILES} ] ; do
+  DEVICE_PACKAGE_CURRENT=${DEVICE_PACKAGES_ARRAY[$i]}
+  echo ${DEVICE_PACKAGE_CURRENT}
+  DEVICE_PACKAGE_CONTINUOUS=$(echo ${DEVICE_PACKAGE_CURRENT} | sed -e "s/${WHICHTAG}/continuous/")
+  echo ${DEVICE_PACKAGE_CONTINUOUS}
+  let i++
+
+done
